ad6ba3336d7ab5df3ef831896fd42d35218e1c48
vuonga1103 <vuonga1103@yahoo.com>
2020-08-31 17:02:25 -0400

    core deliverables completed

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,100 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+
+    form: {
+      date: "",
+      description: "",
+      category: "",
+      amount: "",
+    },
+
+    search: "",
+  };
+
+  componentDidMount() {
+    this.getTransactions();
+  }
+
+  getTransactions = () => {
+    fetch("http://localhost:6001/transactions")
+      .then((response) => response.json())
+      .then((transactions) => {
+        this.allTransactions = transactions;
+        this.setState({ transactions });
+      });
+  };
+
+  // Handle the user's input as they type in the new transaction form, update this.state.form to reflect this
+  handleTransactionInput = (e) => {
+    const name = e.target.name,
+      value = e.target.value;
+
+    const form = {
+      ...this.state.form,
+      [name]: value,
+    };
+
+    this.setState({ form });
+  };
+
+  // Handles the user's submission of the form, makes a fetch post request to server to add new transaction object, sets state to reflect change
+  handleTransactionSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = this.state.form;
+
+    fetch("http://localhost:6001/transactions", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    })
+      .then((response) => response.json())
+      .then((newTransaction) => {
+        const transactions = [...this.state.transactions, newTransaction],
+          form = {
+            date: "",
+            description: "",
+            category: "",
+            amount: "",
+          };
+
+        this.setState({ transactions, form });
+      });
+  };
+
+  // Handles user's input inot searchbar. Only transactions with a description matching the search term should be shown in the transactions table.
+  handleSearchInput = (e) => {
+    const search = e.target.value;
+
+    this.setState({ search });
+
+    const transactions = this.allTransactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(search.toLowerCase())
+    );
+
+    this.setState({ transactions });
+  };
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search
+          search={this.state.search}
+          handleSearchInput={this.handleSearchInput}
+          handleSearchSubmit={this.handleSearchSubmit}
+        />
+        <AddTransactionForm
+          form={this.state.form}
+          handleTransactionInput={this.handleTransactionInput}
+          handleTransactionSubmit={this.handleTransactionSubmit}
+        />
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -2,21 +2,46 @@ import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
   render() {
+    const { date, description, category, amount } = this.props.form;
+
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input
+              type="date"
+              name="date"
+              value={date}
+              onChange={this.props.handleTransactionInput}
+            />
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={description}
+              onChange={this.props.handleTransactionInput}
+            />
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              value={category}
+              onChange={this.props.handleTransactionInput}
+            />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              value={amount}
+              onChange={this.props.handleTransactionInput}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button
+            className="ui button"
+            type="submit"
+            onClick={this.props.handleTransactionSubmit}
+          >
             Add Transaction
           </button>
         </form>
