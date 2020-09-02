e353b3808dd7d5927bf61fcd355b1e60154ecf7b
kelseyjdickson <kelseymedved@gmail.com>
2020-09-02 14:28:42 -0400

    second deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -19,11 +19,22 @@ class AccountContainer extends Component {
       })
     })
   }
+
+//the function helps us access the input from the child, so we send down, get single transaction, then update state since it lives here
+  addedTransaction = (singleTransaction) => {
+    //updating our array of transactions
+    let arrOfTransactions = [...this.state.transactions, singleTransaction]
+    this.setState({
+      transactions: arrOfTransactions
+    })
+  }
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
+        {/* sending down the lobster trap to form so they can send back the input which updates state*/}
+        <AddTransactionForm addedTransaction={this.addedTransaction}/>
+        {/* sending the props to transactionList  */}
         <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,15 +1,76 @@
 import React, { Component } from "react";
+import { enumDeclaration } from "@babel/types";
 
 class AddTransactionForm extends Component {
+  
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: 0
+
+  }
+
+  
+
+  handleInput = (evt) => {
+    this.setState({
+    [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    let transactionFormData = {
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount: this.state.amount
+    }
+    fetch('http://localhost:6001/transactions', {
+      method: "POST",
+      headers: {
+        'content-type': "application/json"
+      },
+      body: JSON.stringify(transactionFormData)
+        })
+      .then(res=> res.json())
+      .then((newTransaction)=>{
+        //console.log(newTransaction)
+        //addedTransaction is the lobster trap that calls down to child to get new info then updates state
+        this.props.addedTransaction(newTransaction)
+      })
+  }
+
+
+
+
+  
+  
   render() {
+
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
-            <input type="number"name="amount"placeholder="Amount"step="0.01"/>
+            
+            <input type="date" name="date" 
+            value={this.state.date}  
+            onChange={this.handleInput}/>
+            
+            <input type="text" name="description" placeholder="Description" 
+            value={this.state.description} 
+            onChange={this.handleInput}/>
+            
+            <input type="text" name="category" placeholder="Category" 
+            value={this.state.category} 
+            onChange={this.handleInput}/>
+            
+            <input type="number"name="amount"placeholder="Amount"step="0.01"
+            value={this.state.amount} 
+            onChange={this.handleInput}/>
+          
           </div>
           <button className="ui button" type="submit">
             Add Transaction
